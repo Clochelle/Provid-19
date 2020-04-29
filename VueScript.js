4f75db0fe3920ff04bbe2b79418b79bb53b11ce3
Clochelle <dadamenthegit config --global user.email dadamenthe@gmail.comgit config --global user.email dadamenthe>
2020-04-29 10:13:35 -0400

    Components questions OK

---
+++ VueScript.js
@@ -0,0 +1,21 @@
+Vue.component('question', { 
+    data: function() {
+        return {selected: false};
+    },
+    props: [
+        'text'
+    ],
+    methods: {
+        functionSelected: function() {
+            this.selected = !this.selected;
+        }
+    },
+    template: `<div><li 
+    v-bind:class="{ 'color-list' : selected }"
+    v-on:click="functionSelected">{{ text }}</li>
+        <p v-if="selected"><slot></slot></p></div>`
+ })
+
+const app = new Vue({
+    el: '#FAQ',
+})
