0c0763e7add0620dfc95f9f8d30935589ffe58ce
clochelle <dadamenthe@gmail.com>
2020-06-05 17:06:42 -0400

    new component + MaJ textes

--- VueScript.js
+++ VueScript.js
@@ -10,22 +10,30 @@ Vue.component('question', {
             this.selected = !this.selected;
         }
     },
-    template: `<div><li 
+    template: `<div><li class="liste-questions"
+    v-bind:class="{ 'color-list' : selected }"
+    v-on:click="functionSelected" v-html="text"></li>
+        <p class="line-height desktop-width" v-if="selected"><slot></slot></p></div>`
+ })
+
+ Vue.component('questionimg', { 
+    data: function() {
+        return {selected: false};
+    },
+    props: [
+        'text'
+    ],
+    methods: {
+        functionSelected: function() {
+            this.selected = !this.selected;
+        }
+    },
+    template: `<div><li class="liste-questions"
     v-bind:class="{ 'color-list' : selected }"
     v-on:click="functionSelected">{{ text }}</li>
-        <p class="line-height desktop-width" v-if="selected"><slot></slot></p></div>`
+        <p class="line-height" v-if="selected"><slot></slot></p></div>`
  })
 
 const app = new Vue({
     el: '#FAQ',
-})
-
-const app2 = new Vue ({
-    el: '#Infos',
-    data: {
-        currentSection: 'Home',
-        level1: 'Home',
-        level2: false,
-        level3: false,
-    },
 })
